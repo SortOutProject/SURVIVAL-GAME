7f48abc3db99d6e52cfdd5e046f9c97496fe50e5
yoat <yoatnic@me.com>
2019-01-05 16:23:19 +0900

    add void project

---
+++ SURVIVAL-GAME/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SURVIVAL-GAME
+//
+//  Created by Takaaki Yoshioka on 2019/01/05.
+//  Copyright © 2019 SORT_OUT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SURVIVAL-GAMETests/SURVIVAL_GAMETests.swift
@@ -0,0 +1,34 @@
+//
+//  SURVIVAL_GAMETests.swift
+//  SURVIVAL-GAMETests
+//
+//  Created by Takaaki Yoshioka on 2019/01/05.
+//  Copyright © 2019 SORT_OUT. All rights reserved.
+//
+
+import XCTest
+@testable import SURVIVAL_GAME
+
+class SURVIVAL_GAMETests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
